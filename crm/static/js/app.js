4d6815f223209429a472ab6d51b843a0546d4ade
wjakubowicz <wjakubowicz@duck.com>
2024-07-08 11:06:21 +0200

    Added missing Bootswatch themes and progress bars below the message boxes

--- crm/static/js/app.js
+++
@@ -1,9 +0,0 @@
-// Select all elements with the class 'message_timer'
-var message_timers = document.querySelectorAll('.message_timer');
-
-// Loop through all elements and set a timeout to hide them
-message_timers.forEach(function(message_timer) {
-    setTimeout(function() {
-        message_timer.style.display = 'none';
-    }, 5000);
-});
